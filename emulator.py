5c121ceee97207667daa21dd28c6849dd7b3b3aa
Yumetaka <liuchangxu2002@qq.com>
2024-02-04 10:42:07 +0800

    2

--- emulator.py
+++ emulator.py
@@ -2,16 +2,25 @@
 潜水器运动模型仿真
 运行本程序以生成epoch次数的潜水器运动的最终位置
 """
+import numpy as np
 
 import Submersible_motion_model
 from tqdm import tqdm
 import csv
 
+# 仿真次数
 epochs = 20000
+
+# 可能的开始时的深度
+depth_start = np.array([0.0, 4000.0])
+
+# 可能的目标深度
+target_depth_ = np.array([2000.0, 4000.0])
 
 position_history = []
 for epoch in tqdm(range(epochs)):
-    position = Submersible_motion_model.emulate()
+
+    position = Submersible_motion_model.emulate(np.random.uniform(depth_start[0], depth_start[1]), np.random.uniform(target_depth_[0], target_depth_[1]))
     position_history.append(position)
 
 # 导出到csv文件
